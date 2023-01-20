ce2ce7e03154513b82d0841c933fe034fb358558
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-01-20 17:18:15 +0530

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import {
+  FaCamera,
+  FaMicrophone,
+  FaSearch,
+  FaUser,
+  FaBell,
+  FaSun,
+  FaShoppingCart,
+} from "react-icons/fa";
+
 const Header = ({ search }) => {
   // const [search,setSearch] = useState("");
   const handleSearch = () => {
@@ -10,7 +17,54 @@ const Header = ({ search }) => {
   return (
     <div className="Header">
       <div className="logoCont">LOGO</div>
-      Hello from header
+      <div className="navbarCont">
+        {/* search form */}
+        <div className="searchForm d-flex justify-content-center align-items-center">
+          <FaSearch />
+          <input type="text" name="searchInp" id="searchInp" className="bg-transparent border-0 p-2 rounded-5" placeholder="Search" />
+          <input type="submit" name="searchBtn" id="searchBtn" />
+        </div>
+
+        {/* Search Icons  */}
+        <div className="searchOptions">
+          <button type="button" className="border-0 rounded-0 bg-transparent">
+            <FaCamera />
+          </button>
+          <button type="button" className="border-0 rounded-0 bg-transparent">
+            <FaMicrophone />
+          </button>
+        </div>
+
+        {/*  user oriented icons */}
+        <div className="userOrientedIconOptions d-flex justify-content-center align-items-center">
+          <button type="button" className="btn border-0 bg-transparent">
+            <FaBell />
+          </button>
+          <button type="button" className="btn border-0 bg-transparent">
+            <FaUser />
+          </button>
+          <button type="button" className="btn border-0 bg-transparent">
+            <FaSun />
+          </button>
+        </div>
+
+        {/*  cart */}
+        <div className="cartContIcon d-flex justify-content-end align-items-center">
+          <span
+            className="text-danger d-flex justify-content-center align-items-center"
+            style={{ fontWeight: "500", fontSize: "18px" }}
+          >
+            0
+          </span>
+          <button
+            type="button"
+            className="btn border-0 "
+            style={{ fontSize: "25px" }}
+          >
+            <FaShoppingCart />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
