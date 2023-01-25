edf4328d1518e2b9891a4f77532c5bbf0613f11d
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-01-25 11:18:36 +0530

    working on redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {StateProvider} from "react-redux";;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider intialState={intialState}>
+       <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
