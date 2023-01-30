2067a8f18643a97199a411ac6a591abfd8ba8378
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-01-30 12:35:19 +0530

    setting reducer using react-redux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {StateProvider} from "react-redux";
+import {StateProvider} from "./redux/StateProvider";
+import reducer, { initialState } from "./reducer";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <StateProvider intialState={intialState}> */}
+    <StateProvider initialState={initialState}>
        <App />
-    {/* </StateProvider> */}
+    </StateProvider>
   </React.StrictMode>
 );
 
