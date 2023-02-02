265a28547c7719cf58b6869c2a1d73684465ab60
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-02-02 13:47:03 +0530

    header Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ import {
   FaSun,
   FaShoppingCart,
 } from "react-icons/fa";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Header = ({ search }) => {
   // const [search,setSearch] = useState("");
@@ -19,25 +19,37 @@ const Header = ({ search }) => {
     <div className="Header">
       <div className="logoCont">LOGO</div>
       <div className="navbarCont">
-        {/* search form */}
-        <div className="searchForm d-flex justify-content-center align-items-center">
-          <FaSearch className="mx-1" style={{fontSize:"19px"}} />
-          <input type="text" name="searchInp" id="searchInp" className="bg-transparent p-2 rounded-5" placeholder="Search" />
-            
-        </div>
+        <div className="d-flex justify-content-center align-items-center">
+          {/* search form */}
+          <div className="searchForm d-flex justify-content-center align-items-center text-primary">
+            <FaSearch className="mx-1" style={{ fontSize: "19px" }} />
+            <input
+              type="text"
+              name="searchInp"
+              id="searchInp"
+              className="bg-transparent p-2 rounded-5"
+              placeholder="Search"
+            />
+          </div>
 
-        {/* Search Icons  */}
-        <div className="searchOptions">
-          <button type="button" className="border-0 rounded-0 bg-transparent">
-            <FaCamera />
-          </button>
-          <button type="button" className="border-0 rounded-0 bg-transparent">
-            <FaMicrophone />
-          </button>
+          {/* Search Icons  */}
+          <div className="searchOptions">
+            <button
+              type="button"
+              className="border-0 rounded-0 bg-transparent text-primary"
+            >
+              <FaCamera />
+            </button>
+            <button
+              type="button"
+              className="border-0 rounded-0 bg-transparent text-primary"
+            >
+              <FaMicrophone />
+            </button>
+          </div>
         </div>
-
         {/*  user oriented icons */}
-        <div className="userOrientedIconOptions d-flex justify-content-center align-items-center">
+        <div className="userOrientedIconOptions d-flex justify-content-end align-items-center">
           <button type="button" className="btn border-0 bg-transparent">
             <FaBell />
           </button>
@@ -58,13 +70,13 @@ const Header = ({ search }) => {
             0
           </span>
           <Link to="/cart">
-          <button
-            type="button"
-            className="btn border-0 "
-            style={{ fontSize: "25px" }}
-          >
-            <FaShoppingCart />
-          </button>
+            <button
+              type="button"
+              className="btn border-0 "
+              style={{ fontSize: "25px" }}
+            >
+              <FaShoppingCart />
+            </button>
           </Link>
         </div>
       </div>
