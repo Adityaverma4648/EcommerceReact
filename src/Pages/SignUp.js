3a3fc1fb1e5319d13942a2ca1248448c5d152153
Aditya verma <adityaverma4648@gmail.com>
2023-04-07 17:28:37 +0530

    bug fixed

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,39 +1,32 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import { useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom";
 
 const intialState = {
       userName : '',
       email : '',
       password : '',
-}
+};
 
 const SignUp = () => {
+
+   const navigate = useNavigate()
    const [formValue, setformValue] = useState(intialState);
    const {userName , email , password} =  formValue;
-  const [status, setStatus] = useState(false);
-  const [dataSent, setDataSent] = useState(false);
 
 
-  const onChecked = (e) => {
-    var passwordInp = document.getElementById("passwordInp");
-    // e.preventDefault();
-    if (e.target.checked) {
-      passwordInp.setAttribute("type", "text");
-    } else {
-      passwordInp.setAttribute("type", "password");
-    }
-  };
-  const onInputChange = (e) =>{
-    let {name , value} = e.target;
-    setformValue({...formValue , [name] : value });
-  }
-  const onSubmission = (e) => {
-       
-  };
+    const onInputChange = (e) =>{
+      let {name , value} = e.target;
+      setformValue({...formValue , [name] : value });
+     }
+     const onSubmission = (e) => {
+         alert(JSON.stringify(formValue));
+     };
 
   return (
     <section className="h-screen w-screen flex justify-center items-center bg-gray-900">
-    <div className="h-3/4 md:h-full">
+    <div className="h-full">
       <div
         className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
         <div
@@ -45,10 +38,11 @@ const SignUp = () => {
         </div>
   
         <div className="md:mb-0 w-11/12 md:w-8/12 lg:w-5/12 xl:w-5/12">
-          <form action="/" className="bg-white rounded-3xl  p-4">
+          <form onSubmit={onSubmission} className="bg-white rounded-3xl  p-4">
+          {/*  heading and other auth options */}
             <div
               className="flex flex-row items-center justify-center lg:justify-center">
-              <p className="mb-0 mr-4 text-lg text-center text-gray-300">Sign in with</p>
+              <p className="mb-0 mr-4 text-lg text-center font-semibold">Sign Up with</p>
               <button
                 type="button"
                 data-te-ripple-init
@@ -94,60 +88,57 @@ const SignUp = () => {
                 </svg>
               </button>
             </div>
-  
+          {/*  form header ends here  */}
             <div
               className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
               <p
-                className="mx-4 mb-0 text-center font-semibold dark:text-white">
+                className="mx-4 mb-0 text-center font-semibold ">
                 Or
               </p>
             </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
+              <label htmlFor="userName" className="text-gray-500" >
+                UserName
+              </label>
               <input
                 type="text"
-                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                className="text-gray-900 placeholder-gray-900 border peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                 id="exampleFormControlInput1"
                 value={userName}
                 name="userName"
                 onChange={(e)=>onInputChange(e)}
                 placeholder="Username" />
-              <label
-                htmlFor="userName"
-                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-                >Email address
-              </label>
+              
             </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
+              <label htmlFor="email" className="text-gray-500" >
+                Email
+              </label>
               <input
                 type="email"
-                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                className="text-gray-900 placeholder-gray-900 border peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                 id="exampleFormControlInput2"
                 value={email}
                 name="email"
                 onChange={(e)=>onInputChange(e)}
                 placeholder="Email address" />
-              <label
-                htmlFor="email"
-                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-                >Email address
-              </label>
+              
             </div>
   
             <div className="relative mb-6" data-te-input-wrapper-init>
+              <label htmlFor="password" className="text-gray-500" >
+                Password
+              </label>
               <input
                 type="password"
-                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                className="text-gray-900 placeholder-gray-900 border peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                 id="exampleFormControlInput22"
                 autoComplete="on"
                 name="password"
                 value={password}
                 onChange={(e)=>{onInputChange(e)}}
                 placeholder="Password" />
-              <label
-                htmlFor="password"
-                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-                >Password
-              </label>
+            
             </div>
   
             <div className="mb-6 flex items-center justify-between px-4">
@@ -168,13 +159,12 @@ const SignUp = () => {
             </div>
   
             <div className="text-center lg:text-left bg-indigo-900 py-2">
-              <button
-                type="button"
+              <input
+                type="submit"
                 className="w-6/12 inline-block rounded bg-primary py-2 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                 data-te-ripple-init
-                data-te-ripple-color="light">
-                Login
-              </button>
+                data-te-ripple-color="light" value="Sign Up">
+              </input>
   
               <p className="mb-0 mt-2 pt-1 text-sm font-semibold text-gray-300">
                 Don't have an account?
