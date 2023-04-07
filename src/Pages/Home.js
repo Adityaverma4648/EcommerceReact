4befc9877b8e36966e08fff0b0daadecac128a7a
Aditya verma <adityaverma4648@gmail.com>
2023-04-07 17:02:46 +0530

    Updated Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Welcome from "../components/Welcome";
 import Category from "../components/Category";
 import Slider from "../components/Slider";
+import axios from "axios";
 
 const Home = () => {
+
+  const fetchData = async()=>{
+    const { data } = await axios.get("http://localhost:7000/");
+    console.log(JSON.stringify(data));
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+
   return (
     <div className="CompContainer">
 
